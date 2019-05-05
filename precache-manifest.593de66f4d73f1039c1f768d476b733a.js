028cfe5b13cea832a2727b4067f244815c4d2a0e
Limeapple <Kevinliu54321@gmail.com>
2019-05-05 16:58:30 -0500

    Updates

---
+++ precache-manifest.593de66f4d73f1039c1f768d476b733a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f0c93bda441fd4c13a",
+    "url": "/Clock/static/css/main.6f4ccf9c.chunk.css"
+  },
+  {
+    "revision": "63f0c93bda441fd4c13a",
+    "url": "/Clock/static/js/main.7800ca29.chunk.js"
+  },
+  {
+    "revision": "897b436228d1692ec50c",
+    "url": "/Clock/static/js/runtime~main.01fd917b.js"
+  },
+  {
+    "revision": "304a7c5bdb32653a453c",
+    "url": "/Clock/static/js/2.b3b8a7ea.chunk.js"
+  },
+  {
+    "revision": "05c47f0ecd43a5e6543dac8d381772c5",
+    "url": "/Clock/index.html"
+  }
+];
